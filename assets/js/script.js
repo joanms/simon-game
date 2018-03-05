9d54b38cbee958ce7159894204422bb4908077b6
Joan Sweeney <joanms@eircom.net>
2018-03-05 10:45:48 +0000

    Edit functions in JavaScript

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,47 @@ var rand = colourArray[Math.floor(Math.random() * 4)];
 var gameSequence = [];
 var userSequence = [];
 var gameScore = 0;
-console.log(gameScore);
+
+
+// Start game
+
+$("#strict").click(function() {
+  $("#strict").addClass("active");
+});
+
+$("#start").click(function() {
+  $(rand).addClass("active");
+  if (rand == "#blue") {
+      $("#blue-sound")[0].play();
+  } else if (rand == "#orange") {
+      $("#orange-sound")[0].play();
+  } else if (rand == "#red") {
+      $("#red-sound")[0].play();
+  } else {
+      $("#yellow-sound")[0].play();
+  }
+  setTimeout(function(){$(rand).removeClass("active");}, 500);
+  gameSequence.push(rand);
+  console.log(gameSequence);
+});
+
+
+// User input
+
+$(".colour").mousedown(function() {
+  $("#"+(this.id)+"-sound")[0].play();
+  $(this).addClass("active");
+  userSequence.push("#" + this.id);
+  console.log(userSequence);
+  sequenceCompare();
+});
+
+$(".colour").mouseup(function() {
+  $(this).removeClass("active");
+});
+
+
+// Compare the user input with the game output
 
 function highlightColours() {
     function setInterval() {
@@ -27,46 +67,19 @@ function highlightColours() {
     }
 }
 
-// Start game
-$("#start").click(function() {
-  $(rand).addClass("active");
-  if (rand == "#blue") {
-      $("#blue-sound")[0].play();
-  } else if (rand == "#orange") {
-      $("#orange-sound")[0].play();
-  } else if (rand == "#red") {
-      $("#red-sound")[0].play();
-  } else {
-      $("#yellow-sound")[0].play();
-  }
-  setTimeout(function(){$(rand).removeClass("active");}, 500);
-  gameSequence.push(rand);
-  console.log(gameSequence);
-});
-
-// User input
-$(".colour").mousedown(function() {
-  $("#"+(this.id)+"-sound")[0].play();
-  $(this).addClass("active");
-  userSequence.push("#" + this.id);
-  console.log(userSequence);
-  sequenceCompare();
-});
-
-$(".colour").mouseup(function() {
-  $(this).removeClass("active");
-});
-
-// Compare the user input with the game output
 function sequenceCompare() {
-  if (userSequence.join == gameSequence.join && userSequence.length == gameSequence.length) {
+  if (userSequence.join == gameSequence.join && userSequence.length == gameSequence.length && gameScore < 20) {
     gameScore++;
     $("#score").text(gameScore);
     userSequence = []; 
+    if (gameScore == 20) {
+      alert("You win!");
+    } else {
     gameSequence.push(rand);
       var arrayLength = gameSequence.length;
       for (var i = 0; i < arrayLength; i++) {
-        gameSequence[i];
+        gameSequence[i].highlightColours();
     }
   } 
+}
 }
