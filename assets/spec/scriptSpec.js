8fdf1d68a3a6146c4876bccd67e5dc889380d92e
Joan Sweeney <joanms@eircom.net>
2018-08-05 16:25:01 +0000

    Add Jasmine tests

---
+++ assets/spec/scriptSpec.js
@@ -0,0 +1,11 @@
+describe("My highlight colours function", function() {       
+it("should exist", function() {
+              expect(highlightColours).toBeDefined();
+            }); 
+      });
+      
+describe("My sequence comparison function", function() {       
+it("should exist", function() {
+              expect(sequenceCompare).toBeDefined();
+            }); 
+      });
